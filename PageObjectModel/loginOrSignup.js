035ae1c2d962e78d6e6260e22d5d270a7705adb2
Mashruf Rohon <mashrufrohon@gmail.com>
2025-06-12 08:37:36 +0600

    added comment in POM

--- PageObjectModel/loginOrSignup.js
+++ PageObjectModel/loginOrSignup.js
@@ -1,26 +1,32 @@
 import { faker } from "@faker-js/faker";
 
 class LoginOrSignup{
-    email = faker.internet.email();
-    name = faker.person.firstName();
+
+    email = faker.internet.email();//creating email for signup
+    name = faker.person.firstName();//creating name for user
     password = 1234;
 
+    //verify the homepage is visible
     homePageVisible(){
         cy.get(".nav.navbar-nav>li:nth-child(1)").should("contain","Home");
     }
 
+    //to click the Signup or login button
     clickLoginOrSignup() {
         cy.contains(".nav.navbar-nav>li", " Signup / Login").click();
     }
 
+    //verify login page is visible
     loginPageVisible(){
         cy.get(".login-form").should("contain","Login to your account");
     }
 
+    //verify signup page is visible
     signupPageVisible(){
         cy.get(".signup-form").should("contain","New User Signup!");
     }
 
+    //create a user from the signup page
     createNewUser() {
         cy.get("input[placeholder='Name']").type(this.name);
         cy.get("input[data-qa='signup-email']").type(this.email);
@@ -44,19 +50,23 @@ class LoginOrSignup{
     }
     
 
+    //enter email and password in the login page
     enterEmailAndPassword(){
         cy.get("input[data-qa='login-email']").type(this.email);
         cy.get("input[data-qa='login-password']").type(this.password);
     }
 
+    //click login button after entering email and password in the login page
     clickLoginButton(){
         cy.get("button[data-qa='login-button']").click();
     }
 
+    //verify the user is logged in
     loginAsUsername(){
         cy.get("li:nth-child(10) a:nth-child(1)").should("contain","Logged in as "+this.name);
     }
- 
+    
+    //logout from user account
     logout(){
         cy.contains(".nav.navbar-nav>li","Logout").click();
     }
